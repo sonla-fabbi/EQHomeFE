111543f07b6c77a04f22d3bbc5802708d79f2e46
hvcgdev <hvcgdev@outlook.com.vn>
2020-07-01 17:22:41 +0700

    update-QLCM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// tab bar
+function quanLyChuyenMuc(chuyenMuc) {
+    var i;
+    var x = document.getElementsByClassName("qLCM");debugger
+    for (i = 0; i < x.length; i++){
+      x[i].style.display = "none";  
+    }debugger
+    document.getElementById(chuyenMuc).style.display = "block";  
+  }
