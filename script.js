e6b401b2a2dd19775f2e2ebbd40d6002d0da8970
hvcgdev <hvcgdev@outlook.com.vn>
2020-07-02 10:31:55 +0700

    update-tao moi chuyen muc

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // tab bar
 function quanLyChuyenMuc(chuyenMuc) {
     var i;
-    var x = document.getElementsByClassName("qLCM");debugger
+    var x = document.getElementsByClassName("qLCM");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
-    }debugger
-    document.getElementById(chuyenMuc).style.display = "block";  
+    }
+    document.getElementById(chuyenMuc).style.display = "flex";
+    document.getElementById(chuyenMuc).style.flexWrap = "wrap";
+    // document.getElementById(chuyenMuc).style.borderBottomColor ="#007bff"
   }
