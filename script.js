bd6ac7d39cb2a55f26886a312bfba07efcb6073a
hvcgdev <hvcgdev@outlook.com.vn>
2020-07-02 14:41:03 +0700

    update-QL BDS

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function quanLyChuyenMuc(chuyenMuc) {
     document.getElementById(chuyenMuc).style.flexWrap = "wrap";
     // document.getElementById(chuyenMuc).style.borderBottomColor ="#007bff"
   }
+
+  function quanLyBDS(BDS) {
+    var i;
+    var x = document.getElementsByClassName("batDongSan");
+    for (i = 0; i < x.length; i++){
+      x[i].style.display = "none";  
+    }
+    document.getElementById(BDS).style.display = "flex";
+    // document.getElementById(chuyenMuc).style.borderBottomColor ="#007bff"
+  }
+
