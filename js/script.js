37983368e414256c357b93c24e916078bf7ae79a
hvcgdev <hvcgdev@outlook.com.vn>
2020-07-07 13:53:42 +0700

    update-raw-version

--- js/script.js
+++ js/script.js
@@ -1,66 +1,114 @@
 // tab bar
-function quanLyChuyenMuc(chuyenMuc) {
-    var i;
+function quanLyChuyenMuc(evt, chuyenMuc) {
     var x = document.getElementsByClassName("tab-content");
-    for (i = 0; i < x.length; i++){
+    for (var i = 0; i < x.length; i++){
       x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
     }
     document.getElementById(chuyenMuc).style.display = "flex";
     document.getElementById(chuyenMuc).style.flexWrap = "wrap";
+    evt.currentTarget.className += " active"; 
   }
 
-  function quanLyBDS(BDS) {
+  function quanLyBDS(evt, BDS) {
     var i;
     var x = document.getElementsByClassName("tab-content");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
     }
     document.getElementById(BDS).style.display = "flex";
+    evt.currentTarget.className += " active";
   }
 
-  function quanLyNguoiDung(nD) {
+  function quanLyNguoiDung(evt, nD) {
     var i;
     var x = document.getElementsByClassName("tab-content");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
     }
     document.getElementById(nD).style.display = "flex";
+    evt.currentTarget.className += " active";
   }
 
-  function quanLyHuongNha(hN) {
+  function quanLyHuongNha(evt, hN) {
     var i;
     var x = document.getElementsByClassName("tab-content");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
     }
     document.getElementById(hN).style.display = "flex";
+    evt.currentTarget.className += " active";
   }
 
-  function quanLyLaiSuat(lS) {
+  function quanLyLaiSuat(evt, lS) {
     var i;
     var x = document.getElementsByClassName("tab-content");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
     }
     document.getElementById(lS).style.display = "flex";
+    evt.currentTarget.className += " active";
   }
 
-  function gioiThieu(gT) {
+  function quanLyKyGui(evt, kG) {
     var i;
     var x = document.getElementsByClassName("tab-content");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
+    }
+    document.getElementById(kG).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
+
+  function gioiThieu(evt, gT) {
+    var i;
+    var x = document.getElementsByClassName("tab-content");
+    for (i = 0; i < x.length; i++){
+      x[i].style.display = "none";  
+    }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
     }
     document.getElementById(gT).style.display = "flex";
+    evt.currentTarget.className += " active";
   }
 
-  function tinBDS(tBDS) {
+  function tinBDS(evt, tBDS) {
     var i;
     var x = document.getElementsByClassName("tab-content");
     for (i = 0; i < x.length; i++){
       x[i].style.display = "none";  
     }
+    var z = document.getElementsByClassName("tab-bar-link");
+    for (i = 0; i < z.length; i++) {
+      z[i].className = z[i].className.replace(" active", "");
+    }
     document.getElementById(tBDS).style.display = "flex";
+    evt.currentTarget.className += " active";
   }
 
   function showDetail(detail) {
@@ -71,5 +119,6 @@ function quanLyChuyenMuc(chuyenMuc) {
     }
     document.getElementById(detail).style.display = "flex";
   }
-
+  
+  document.getElementById("defaultOpen").click();
 
